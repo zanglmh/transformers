eb3bb4620c1f33399985c71d98f4781304c78bf6
zanglmh <55202854+zanglmh@users.noreply.github.com>
2022-11-29 13:26:48 -0500

    Initial commit

---
+++ src/main/java/transformers/maven_archetype_quickstart/App.java
@@ -0,0 +1,7 @@
+package transformers.maven_archetype_quickstart;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/transformers/maven_archetype_quickstart/AppTest.java
@@ -0,0 +1,13 @@
+package transformers.maven_archetype_quickstart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
