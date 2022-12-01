01d55a8d378e79cbaf28153cbb9e2d4385c47633
ladeane00 <ladeane00@gmail.com>
2022-12-01 14:05:00 -0500

    Contract
    Initial Interface Contract

---
+++ src/main/java/transformers/maven_archetype_quickstart/TranformationException.java
@@ -0,0 +1,11 @@
+package transformers.maven_archetype_quickstart;
+
+public class TranformationException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+
+    public TranformationException(String message) {
+        super(message);
+    }
+
+}
