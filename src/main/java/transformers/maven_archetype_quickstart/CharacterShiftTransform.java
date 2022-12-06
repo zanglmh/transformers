6ff4a7bb9248b06da08afb58821e4569715bde4c
zanglmh <55202854+zanglmh@users.noreply.github.com>
2022-12-06 13:39:06 -0500

    Organized

--- src/main/java/transformers/maven_archetype_quickstart/CharacterShiftTransform.java
+++ src/main/java/transformers/maven_archetype_quickstart/CharacterShiftTransform.java
@@ -4,19 +4,14 @@ public class CharacterShiftTransform implements Transform{
 
     @Override
     public String forwardString(String input) throws TranformationException {
-        int len = input.length();
         String shifted = "";
-        char[] chars = new char[len];
-
-        for(int i = 0 ; i < len ; i++){
-            char c = input.charAt(i);
-            chars[i] = c;
-        }
 
         for(char c : input.toCharArray()){
             if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
                 if(c == 'z'){
                     shifted += 'a';
+                }else if(c == 'Z') {
+                    shifted += 'A';
                 }else{
                     shifted += ++c;
                 }
@@ -29,19 +24,14 @@ public class CharacterShiftTransform implements Transform{
 
     @Override
     public String reverseTranform(String input) throws TranformationException {
-        int len = input.length();
         String shifted = "";
-        char[] chars = new char[len];
-
-        for(int i = 0 ; i < len ; i++){
-            char c = input.charAt(i);
-            chars[i] = c;
-        }
 
         for(char c : input.toCharArray()){
             if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
                 if(c == 'a'){
                     shifted += 'z';
+                }else if(c == 'A') {
+                    shifted += 'Z';
                 }else{
                     shifted += --c;
                 }
